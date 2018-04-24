e17e5fafa436a86b8c6fc794876f7f3296868569
Florian BARBARE <florian.barbare@gmail.com>
2018-04-24 09:37:20 +0100

    adding functionality of custom webpackJsonp function name based on project name

--- packages/next-react-app-scripts/config.js
+++ packages/next-react-app-scripts/config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const withCSS = require('@zeit/next-css');
+const camelCase = require('camelcase');
+
+const paths = require('./utils/paths');
+const packageJSON = require(paths.packageJSON);
 
 module.exports = withCSS({
   distDir: path.join('..', '..', 'build'),
@@ -8,5 +12,13 @@ module.exports = withCSS({
     process.env.NEXT_BUILD_ID = Date.now();
 
     return process.env.NEXT_BUILD_ID;
+  },
+  webpack: function(config, options) {
+    if (packageJSON.name) {
+      config.output.jsonpFunction =
+        'webpackJsonp' + camelCase(packageJSON.name, { pascalCase: true });
+    }
+
+    return config;
   }
 });
