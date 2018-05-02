e0e9e48b1ee3161db62817cdc841e1aa43998a01
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2018-05-02 12:39:50 +0200

    add polyfill entry if it exists

--- packages/next-react-app-scripts/config.js
+++ packages/next-react-app-scripts/config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const withCSS = require('@zeit/next-css');
 const camelCase = require('camelcase');
 
@@ -13,11 +14,22 @@ module.exports = withCSS({
 
     return process.env.NEXT_BUILD_ID;
   },
-  webpack: function(config, options) {
+  webpack: function (config, options) {
     if (packageJSON.name) {
       config.output.jsonpFunction =
         'webpackJsonp' + camelCase(packageJSON.name, { pascalCase: true });
     }
+
+    const originalEntry = config.entry;
+    config.entry = () => {
+      return originalEntry().then(entries => {
+        if (entries['main.js'] && fs.existsSync(paths.polyfills)) {
+          entries['main.js'].unshift(paths.polyfills);
+        }
+
+        return entries;
+      });
+    };
 
     return config;
   }
